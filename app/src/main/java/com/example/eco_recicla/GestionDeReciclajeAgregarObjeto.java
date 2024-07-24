99cc32dc687357219a62197befc7d9a08a661228
JonathanDuarteGomez <camiloduarteg@gmail.com>
2024-07-24 07:05:16 -0500

    Se corrigio el problema de la tabla, falta almacenar los datos que se registran y mostrarlos pero en cuanto a la visual se mejoro en la clase de Gestion de reciclaje agregar objeto

--- app/src/main/java/com/example/eco_recicla/GestionDeReciclajeAgregarObjeto.java
+++ app/src/main/java/com/example/eco_recicla/GestionDeReciclajeAgregarObjeto.java
@@ -2,10 +2,12 @@ package com.example.eco_recicla;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 import android.widget.TableLayout;
@@ -18,21 +20,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.eco_recicla.Enums.CategoriasDeReciclaje;
+import com.example.eco_recicla.Enums.TiposDeDocumentos;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GestionDeReciclajeAgregarObjeto extends AppCompatActivity {
+    //bandera de la tabla
+    private boolean flag = false;
     private String[] header;
     private ArrayList<String[]> rows;
     private Button btnSiguiente;
-
+    private Button btnIrAMenuPrincipal;
     TableDynamic tableDynamic;
     private TableLayout tablaObjetosAgregados;
-    //private Spinner spinnerProblemas;
-    Spinner spinnerSeleccionDeDireccion;
-    Spinner spinnerGrupo;
-    Spinner spinnerTipo;
+    //private Spinner spinnedirecciones;
+    private Spinner spinnerSeleccionDeDireccion;
+    private Spinner spinnerGrupo;
+    private Spinner spinnerTipo;
+
+    //opciones del spinner
+    String[]direcciones = new String[3];
+
 
 
     @Override
@@ -41,7 +50,16 @@ public class GestionDeReciclajeAgregarObjeto extends AppCompatActivity {
         setContentView(R.layout.activity_gestion_de_reciclaje_agregar_objeto);
 
         btnSiguiente = (Button) findViewById(R.id.btnSiguiente);
+        btnIrAMenuPrincipal = (Button) findViewById(R.id.btnIrAMenuPrincipal);direcciones[0] = "Seleccione Direccion";direcciones[1] = "Direccion 1";direcciones[2] = "Direccion 2";
+
         //navegacion entre pantallas
+        btnIrAMenuPrincipal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(GestionDeReciclajeAgregarObjeto.this, MenuPrincipal.class);
+                startActivity(next);
+                finish();
+            }});
         btnSiguiente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,15 +80,26 @@ public class GestionDeReciclajeAgregarObjeto extends AppCompatActivity {
         tableDynamic = new TableDynamic(tablaObjetosAgregados,getApplicationContext());
         tableDynamic.addHeader(header);
         //## se necesita encontrar la forma de no tenerque llamar a addData  y que la tabla se cree una sola vez
-        tableDynamic.addData(getProducto());
-        tableDynamic.linearColor();
+        //tableDynamic.addData(getProducto());
+
 
         agregarObjeto.setOnClickListener(new View.OnClickListener(){
             public void onClick (View v){
-                //agregar objeto
+                if(!flag){//si la tabla no esta creada
 
-                saveItem();
-                //tableDynamic.linearColor();
+                    if(obtenerInformacionSegunSpinner()==true){//si la informacion es correcta
+                        tableDynamic.addData(getProducto());
+                        tableDynamic.linearColor();
+                        saveItem();
+                        flag = true;
+                        Toast.makeText(GestionDeReciclajeAgregarObjeto.this, "Datos ingresados correctamente", Toast.LENGTH_SHORT).show();
+                    }else if(obtenerInformacionSegunSpinner()==false) {//si la informacion es incorrecta
+                        Toast.makeText(GestionDeReciclajeAgregarObjeto.this, "Datos ingresados incorrectamente", Toast.LENGTH_SHORT).show();
+                        flag = false;
+                    }
+                }else if(flag==true){//si la tabla esta creada
+                    saveItem();
+                }
             }
         });
 
@@ -81,6 +110,32 @@ public class GestionDeReciclajeAgregarObjeto extends AppCompatActivity {
 
         //llama a categoria funcion
         ConfiguracionCategoriasDeReciclaje();
+
+
+
+
+        //configurar el Spiner con Direcciones del usuario
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,direcciones);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSeleccionDeDireccion.setAdapter(adapter);
+        //configurar el listener del spinner
+        spinnerSeleccionDeDireccion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if(position==0){
+                    Log.i("Direccion: ",direcciones[position].toString());
+                }else {
+                    Log.i("Direccion: ",direcciones[position].toString());
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Log.i("Tipo de documento", "Seleccione Tipo de documento");
+            }
+        });
     }
     private void ConfiguracionCategoriasDeReciclaje(){
         //categorias de reciclaje
@@ -116,7 +171,7 @@ public class GestionDeReciclajeAgregarObjeto extends AppCompatActivity {
     private void configuracionSppinerTipo(CategoriasDeReciclaje categoria){
         //subcategorias de reciclaje
         List<String> subcategories = new ArrayList<>();
-        subcategories.add("Seleccione la Subcategoría");
+        subcategories.add("Seleccione la Subcategoria");
         subcategories.addAll(categoria.getSubcategories().keySet());
 
         ArrayAdapter<String> subcategoryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, subcategories);
@@ -148,11 +203,42 @@ public class GestionDeReciclajeAgregarObjeto extends AppCompatActivity {
     }
 
     private ArrayList<String[]> getProducto() {
-        rows.add(new String[]{"1","Cartón","40","500","20000","10","10","20000"});
-        rows.add(new String[]{"2","Plástico","55","550","30250","825","835","50250"});
+        //rows.add(new String[]{"","","","","","","",""});
+        //rows.add(new String[]{"1","Cartón","40","500","20000","10","10","20000"});
+        //rows.add(new String[]{"2","Plástico","55","550","30250","825","835","50250"});
         //rows.add(new String[]{" "," "," "," "," ","Total :"," C:835 ","$50250"});
         return rows;
     }
+
+    private boolean obtenerInformacionSegunSpinner() {
+        try {
+            if (spinnerSeleccionDeDireccion == null || spinnerGrupo == null || spinnerTipo == null) {
+                Log.e("Error", "Uno o más spinners son nulos");
+                return false;
+            }
+
+            String direccionSeleccionada = spinnerSeleccionDeDireccion.getSelectedItem().toString();
+            String grupoSeleccionado = spinnerGrupo.getSelectedItem().toString();
+            String tipoSeleccionado = spinnerTipo.getSelectedItem().toString();
+            EditText editText = findViewById(R.id.EditTextKG);
+
+            if (!direccionSeleccionada.equals("Seleccione Direccion") &&
+                    !grupoSeleccionado.equals("Seleccione la Categoria")&& !editText.getText().toString().equals("")
+                    && !tipoSeleccionado.equals("Seleccione la Subcategoria")
+                ) {
+
+                Log.i("Info Direccion", direccionSeleccionada);
+                return true;
+            } else {
+                Log.i("Info Direccion", "No se ha seleccionado ninguna opción válida");
+                return false;
+            }
+        } catch (Exception e) {
+            Log.e("Error", "Error al obtener información del spinner", e);
+            return false;
+        }
+    }
+
 
 
 }
--- app/src/main/java/com/example/eco_recicla/GestionDeReciclaje_AgregarSolicitudDeRecogida.java
+++ app/src/main/java/com/example/eco_recicla/GestionDeReciclaje_AgregarSolicitudDeRecogida.java
@@ -2,10 +2,14 @@ package com.example.eco_recicla;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +23,12 @@ public class GestionDeReciclaje_AgregarSolicitudDeRecogida extends AppCompatActi
     private Button btnDate;
     private Button btnIrAGestionDeReciclajeAgregarObjeto;
     private ImageButton btnCancelar;
+    private Spinner spinnerSeleccionDeEmpresa;
+    private Spinner spinnerPlacaVehiculo;
+    private Spinner spinnerConductor;
+    private String[] nombreEmpresas = new String[3];
+    private String[] nombreVehiculos = new String[3];
+    private String[] nombreConductores = new String[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +38,15 @@ public class GestionDeReciclaje_AgregarSolicitudDeRecogida extends AppCompatActi
         btnDate = findViewById(R.id.datePickerButton);
         btnIrAGestionDeReciclajeAgregarObjeto = findViewById(R.id.btnIrAGestionDeReciclajeAgregarObjeto);
         btnCancelar = findViewById(R.id.btnCancelar);
+        spinnerSeleccionDeEmpresa = findViewById(R.id.spinnerSeleccionDeEmpresa);
+        nombreEmpresas[0] = "Seleccione Empresa";nombreEmpresas[1] = "Empresa 1";nombreEmpresas[2] = "Empresa 2";
+        spinnerPlacaVehiculo = findViewById(R.id.spinnerPlacaVehiculo);
+        nombreVehiculos[0] = "Seleccione Vehículo";nombreVehiculos[1] = "Vehículo 1";nombreVehiculos[2] = "Vehículo 2";
+        spinnerConductor = findViewById(R.id.spinnerConductor);
+        nombreConductores[0] = "Seleccione Conductor";nombreConductores[1] = "Conductor 1";nombreConductores[2] = "Conductor 2";
 
-        //
-        //navegacion entre pantallas
+
+        // Navegación entre pantallas
         btnIrAGestionDeReciclajeAgregarObjeto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,8 +65,7 @@ public class GestionDeReciclaje_AgregarSolicitudDeRecogida extends AppCompatActi
         });
 
         // Configurar el OnClickListener para el botón de los picker
-        btnDate.setOnClickListener(v ->
-                showDatePickerDialog());
+        btnDate.setOnClickListener(v -> showDatePickerDialog());
 
         Button timePickerButton = findViewById(R.id.timePickerButton);
         timePickerButton.setOnClickListener(new View.OnClickListener() {
@@ -60,13 +75,73 @@ public class GestionDeReciclaje_AgregarSolicitudDeRecogida extends AppCompatActi
             }
         });
 
+        // Configurar el spinnerSeleccionDeEmpresa
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombreEmpresas);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerSeleccionDeEmpresa.setAdapter(adapter);
+
+        // Configurar el listener del spinner
+        spinnerSeleccionDeEmpresa.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    obtenerInformacionSegunSpinner();
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                obtenerInformacionSegunSpinner();
+            }
+        });
+        // Configurar el spinnerPlacaVehiculo
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombreVehiculos);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPlacaVehiculo.setAdapter(adapter1);
+
+        //configuracion del listener spinnerPlacaVehiculo
+        spinnerPlacaVehiculo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    obtenerInformacionSegunSpinner();
+                };
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                obtenerInformacionSegunSpinner();
+            }
+        });
+
+        //configurar el spinnerConductor
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, nombreConductores);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerConductor.setAdapter(adapter2);
+        //configuracion del listener spinnerConductor
+        spinnerConductor.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0) {
+                    obtenerInformacionSegunSpinner();
+                };
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                obtenerInformacionSegunSpinner();
+            }
+        });
+
+
+
     }
 
-    //Funciones de los picker
+    // Funciones de los picker
     public void showDatePickerDialog() {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
     }
+
     public void showTimePickerDialog() {
         DialogFragment newFragment = new TimePickerFragment();
         newFragment.show(getSupportFragmentManager(), "timePicker");
@@ -78,16 +153,32 @@ public class GestionDeReciclaje_AgregarSolicitudDeRecogida extends AppCompatActi
         String year_string = Integer.toString(year);
         String dateMessage = (month_string + "/" + day_string + "/" + year_string);
         TextView dateTextView = findViewById(R.id.etDate);
-        dateTextView.setText( dateMessage);
+        dateTextView.setText(dateMessage);
     }
+
     public void processTimePickerResult(int hourOfDay, int minute) {
         String hour_string = Integer.toString(hourOfDay);
         String minute_string = Integer.toString(minute);
         String timeMessage = (hour_string + ":" + minute_string);
         TextView timeTextView = findViewById(R.id.timeTextView);
-        timeTextView.setText( timeMessage);
+        timeTextView.setText(timeMessage);
     }
 
+    // Función para obtener la información basada en el spinner activo
+    private void obtenerInformacionSegunSpinner() {
+        String empresaSeleccionada = spinnerSeleccionDeEmpresa.getSelectedItem().toString();
+        String vehiculoSeleccionado = spinnerPlacaVehiculo.getSelectedItem().toString();
+        String conductorSeleccionado = spinnerConductor.getSelectedItem().toString();
 
-
+        // Verificar cuál spinner tiene la información seleccionada
+        if (!empresaSeleccionada.equals("Seleccione Empresa")) {
+            Log.i("Info Empresa", empresaSeleccionada);
+        } else if (!vehiculoSeleccionado.equals("Seleccione Vehículo")) {
+            Log.i("Info Vehículo", vehiculoSeleccionado);
+        } else if (!conductorSeleccionado.equals("Seleccione Conductor")) {
+            Log.i("Info Conductor", conductorSeleccionado);
+        } else {
+            Log.i("Info", "No se ha seleccionado ninguna opción válida");
+        }
+    }
 }
