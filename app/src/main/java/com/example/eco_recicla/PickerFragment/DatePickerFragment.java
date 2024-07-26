80a4671d1b90dddcbda45b269b6451225a03b2cd
JonathanDuarteGomez <camiloduarteg@gmail.com>
2024-07-25 21:46:31 -0500

    Se ajusto la vista solicitud de recogida y se modificaron los Piker con el fin de que se cumplan reglas de negocio

--- app/src/main/java/com/example/eco_recicla/PickerFragment/DatePickerFragment.java
+++ app/src/main/java/com/example/eco_recicla/PickerFragment/DatePickerFragment.java
@@ -1,7 +1,6 @@
 package com.example.eco_recicla.PickerFragment;
 
 import android.widget.DatePicker;
-
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.os.Bundle;
@@ -11,8 +10,7 @@ import com.example.eco_recicla.GestionDeReciclaje_AgregarSolicitudDeRecogida;
 
 import java.util.Calendar;
 
-public class DatePickerFragment extends DialogFragment
-        implements DatePickerDialog.OnDateSetListener {
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -23,14 +21,18 @@ public class DatePickerFragment extends DialogFragment
         int day = c.get(Calendar.DAY_OF_MONTH);
 
         // Crear una nueva instancia de DatePickerDialog y devolverla
-        return new DatePickerDialog(getActivity(), this, year, month, day);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), this, year, month, day);
+
+        // Establecer la fecha mínima en el DatePickerDialog a hoy
+        c.add(Calendar.DAY_OF_MONTH, 1);
+        datePickerDialog.getDatePicker().setMinDate(c.getTimeInMillis());
+
+        return datePickerDialog;
     }
 
+    @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
         GestionDeReciclaje_AgregarSolicitudDeRecogida activity = (GestionDeReciclaje_AgregarSolicitudDeRecogida) getActivity();
         activity.processDatePickerResult(year, month, dayOfMonth);
     }
 }
-
-
