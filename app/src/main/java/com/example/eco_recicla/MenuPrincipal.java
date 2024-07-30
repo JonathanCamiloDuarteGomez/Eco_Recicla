12a073329c94d2213d2a5274fffb4c7a4ddfbe94
Jonathan Camilo Duarte Gomez <142137108+JonathanCamiloDuarteGomez@users.noreply.github.com>
2024-07-30 13:24:02 -0500

    Camilo (#18)
    * Camilo (#17)
    
    * creacion de la app base
    
    * Aqui esta mi parte, pantallas de menu principal y historial
    
    * cambio de rama
    
    * Proyecto inicial con login colores y archivos jpg
    
    * Camilo (#9)
    
    * Aqui esta mi parte, pantallas de menu principal y historial
    
    * Se agrego un boton que faltaba en la pantalla historial
    
    * Se modificaron los nombres de las clases para evitar confusión
    
    ---------
    
    Co-authored-by: JonathanDuarteGomez <camiloduarteg@gmail.com>
    
    * Se agrego un boton que faltaba en la pantalla historial
    
    * Se modificaron los nombres de las clases para evitar confusión
    
    * e realizan acciones de botones faltantes
    
    * Se agrega vista Premios (#11)
    
    * hola
    
    * Se crearon las vistas activity_datos_personales.xml , activity_menu_informacion_usuario.xml , con sus respectivas clases .java y la navegacion entre ellas, en la activity_crear_cuenta se agregaron campos y se adiciono un spinner para el tipo de CC
    
    * Camilo (#12)
    
    * Aqui esta mi parte, pantallas de menu principal y historial
    
    * Se agrego un boton que faltaba en la pantalla historial
    
    * Se modificaron los nombres de las clases para evitar confusión
    
    * hola
    
    * Se crearon las vistas activity_datos_personales.xml , activity_menu_informacion_usuario.xml , con sus respectivas clases .java y la navegacion entre ellas, en la activity_crear_cuenta se agregaron campos y se adiciono un spinner para el tipo de CC
    
    ---------
    
    Co-authored-by: JonathanDuarteGomez <camiloduarteg@gmail.com>
    
    * Creacion de la pantalla agregar Objeto, modulo de gestion de reciclaje
    
    * Se dejaron los spinners de gestion de reciclaje de categorias y sub categorias de reciclaje, hace falta el spinner de direccion y los btn de navegacion
    
    * Se agrego pantalla agregar solicitiud de recogida,hacen falta configurar los spinners y algunas cosas de navegacion
    
    ---------
    
    Co-authored-by: JonathanDuarteGomez <camiloduarteg@gmail.com>
    Co-authored-by: Consultoria KFW <gusefi1992@gmail.com>
    Co-authored-by: Guillermo Segura <115810750+gusefi1992@users.noreply.github.com>
    Co-authored-by: Ivricoca <ivricoca@gmail.com>
    
    * 18/07/2024 Diseño de la app al 75%
    
    * ok
    
    * codigo modificado con el profesor
    
    * arreglos del profesor a los consejos
    
    ---------
    
    Co-authored-by: JonathanDuarteGomez <camiloduarteg@gmail.com>
    Co-authored-by: Consultoria KFW <gusefi1992@gmail.com>
    Co-authored-by: Guillermo Segura <115810750+gusefi1992@users.noreply.github.com>
    Co-authored-by: Ivricoca <ivricoca@gmail.com>

--- app/src/main/java/com/example/eco_recicla/MenuPrincipal.java
+++ app/src/main/java/com/example/eco_recicla/MenuPrincipal.java
@@ -1,13 +1,33 @@
 package com.example.eco_recicla;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MenuPrincipal extends AppCompatActivity {
+    private TextView textviewDate;
+
+    private String[] Tips= {
+            "Ahorra energía apagando las luces que no necesitas.",
+            "Desconecta los electrodomésticos que no estás usando.",
+            "Utiliza bombillas LED para reducir el consumo de energía.",
+            "Apaga tu computadora cuando no la estés usando.",
+            "Aprovecha la luz natural durante el día.",
+            "Mantén el refrigerador bien cerrado para ahorrar energía.",
+            "Usa el aire acondicionado con moderación.",
+            "Lava tu ropa con agua fría para ahorrar energía.",
+            "Revisa tus aparatos eléctricos para evitar fugas de energía.",
+            "Plancha tu ropa en una sola sesión para ahorrar energía."
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +39,35 @@ public class MenuPrincipal extends AppCompatActivity {
         ImageButton btnEstadisticasDeReciclaje = findViewById(R.id.buttonEstadisticasDeReciclaje);
         ImageButton btnDatosPersonales = findViewById(R.id.buttonInformacionDeUsuario);
         ImageButton btnSalir = findViewById(R.id.floatingActionButton2);
+        TextView textviewDate =findViewById(R.id.data);
+        //inicializar sharePreference
+        SharedPreferences preferences = getSharedPreferences("DailyTips",MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        //Verificar los consejos ya guardados
+
+        boolean isInitialized = preferences.getBoolean("isInitialized",false);
+
+        if(!isInitialized){
+            for(int i=0; i < Tips.length;i ++){
+                editor.putString("tip"+ i,Tips[i]);
+            }
+            editor.putBoolean("isInitialized",true);
+            editor.apply();
+        }
+
+//EEEE dia d Fecha  MMMM mes yyyy año
+        SimpleDateFormat sdf = new SimpleDateFormat("EEEE,d 'de' MMMM 'de' yyyy", new Locale("es", "CO"));
+
+//almacena la cadena de la fecha y formateda
+        String currentDate = sdf.format(new Date());
+        textviewDate.setText(currentDate);
 
         //en la siguiente funcion se ingresa el bton y la clase hacia donde se va
 
         cambioDePantalla(btnGestionDeRe, GestionDeReciclajeAgregarObjeto.class);
         cambioDePantalla(btnIncentivosRecompensas, Premios.class);
-        cambioDePantalla(btnEstadisticasDeReciclaje, HistorialActivity.class);
+        cambioDePantalla(btnEstadisticasDeReciclaje, Estadisticas.class);
         cambioDePantalla(btnDatosPersonales, MenuInformacionUsuario.class);
         cambioDePantalla(btnSalir, MainActivity.class);//cambiar a login
         //no olvidar reemplazar la clase pa donde se dirije, no son HistorialActivity
